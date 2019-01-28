99f514615a25804a51f2496150cc9856969e4a61
Ilya Ryabukhin <ryabuily@fit.cvut.cz>
2019-01-28 16:44:22 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AMC_SharpIntro
+{
+    public class Animal
+    {
+        private string Name { get; set;}
+        protected int Age { get; set; }
+
+        public Animal()
+        {
+
+        }
+        public Animal(string name, int age)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+
+    }
+    public class Cow : Animal
+    {
+
+        public Cow() : base()
+        {
+            var a = this.Age;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 0;
+                int tmp = 2 / a;
+                Console.WriteLine(tmp);
+            }
+            catch { Console.WriteLine("smth went wrong"); }
+
+        }
+    }
+}
